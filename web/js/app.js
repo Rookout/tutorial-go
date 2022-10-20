eb1e50b5b131e257e5ff7e8869ad234f115d3331
Ryan Hoffman <hopemanryan@gmail.com>
2022-10-20 14:11:48 +0300

    remove all

--- web/js/app.js
+++ web/js/app.js
@@ -56,6 +56,12 @@ $().ready(() => {
                         break;
                 }
             },
+            removeAll() {
+                const action = $.ajax('/todos', {
+                    method: 'DELETE'
+                });
+                this.reloadOnFinish(action);
+            },
             clearCompleted(e) {
                 const action = $.ajax('/todos/clear_completed', {
                     method: 'DELETE'
